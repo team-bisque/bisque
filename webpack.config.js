f4e55785cfb86041329ad56cf5b79c17b17187f8
milocosmopolitan <milocosmopolitan@gmail.com>
2017-01-25 15:39:48 -0500

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var webpack = require("webpack"),
+    path = require("path"),
+    fileSystem = require("fs"),
+    env = require("./utils/env"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    WriteFilePlugin = require("write-file-webpack-plugin");
+
+// load the secrets
+var alias = {};
+
+var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
+
+if (fileSystem.existsSync(secretsPath)) {
+  alias["secrets"] = secretsPath;
+}
+
+module.exports = {
+  entry: {
+    popup: path.join(__dirname, "src", "js", "popup.js"),
+    options: path.join(__dirname, "src", "js", "options.js"),
+    background: path.join(__dirname, "src", "js", "background.js")
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.(js|jsx)$/, loader: "babel" },
+      { test: /\.css$/, loaders: ["style", "css"] }
+    ]
+  },
+  resolve: {
+    alias: alias,
+    extensions: ["", ".js", ".jsx", ".css"]
+  },
+  plugins: [
+    // expose and write the allowed env vars on the compiled bundle
+    new webpack.DefinePlugin({ "process.env": JSON.stringify(env) }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "popup.html"),
+      filename: "popup.html",
+      chunks: ["popup"]
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "options.html"),
+      filename: "options.html",
+      chunks: ["options"]
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "background.html"),
+      filename: "background.html",
+      chunks: ["background"]
+    }),
+    new WriteFilePlugin()
+  ]
+};
