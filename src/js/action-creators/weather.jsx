ff5a5c92cb63c3e4b35cdfd1bb8d19f307629266
bcnichols3 <bcnichols3@gmail.com>
2017-01-30 11:29:49 -0500

    weather making icons off zip

--- src/js/action-creators/weather.jsx
+++ src/js/action-creators/weather.jsx
@@ -3,6 +3,10 @@
 import axios from 'axios';
 import {weatherKey} from '../apiKeys';
 
+import ChromePromise from 'chrome-promise';
+
+const chromep = new ChromePromise();
+
 import {
   RECEIVE_WEATHER
 } from '../constants';
@@ -16,9 +20,7 @@ export const receiveWeather = (weather) =>
 export const fetchWeather = (zip) =>
   dispatch => {
     axios.get(`${openweather}&zip=${zip},us`)
-      .then(res => res.data)
-      .then(data => {
-        dispatch(receiveWeather(data));
-      })
-      .catch(err => console.error('Problem fetching weather', err));
+    .then(res => res.data)
+    .then(data => dispatch(receiveWeather(data)))
+    .catch(err => console.error('Problem fetching weather', err));
   };
