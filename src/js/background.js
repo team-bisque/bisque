7aea3b624503c78b20b47e38a51245daf9e85a0e
bcnichols3 <bcnichols3@gmail.com>
2017-01-31 13:10:15 -0500

    firebase is now linked up

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,21 @@
 'use strict';
 
-import { wrapStore }  from 'react-chrome-redux';
-import store          from './store';
+// Initialize Firebase
+import {firebaseKey} from './apiKeys';
+
+firebase.initializeApp({
+  apiKey: firebaseKey,
+  authDomain: 'go-outside-76d86.firebaseapp.com',
+  databaseURL: 'https://go-outside-76d86.firebaseio.com',
+  storageBucket: 'go-outside-76d86.appspot.com',
+  messagingSenderId: '75953039302'
+});
+
+console.log(firebase);
 
 // Provides redux store to newtab.js
+import { wrapStore }  from 'react-chrome-redux';
+import store          from './store';
 wrapStore(store, {portName: '1337'});
 
 // Creates a new tab when tray icon is clicked
