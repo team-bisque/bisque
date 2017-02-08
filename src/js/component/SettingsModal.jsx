4642ec6251cdd608b77796a9d42dfc1a317cb439
milocosmopolitan <milocosmopolitan@gmail.com>
2017-02-07 19:57:59 -0500

    user setting polished

--- src/js/component/SettingsModal.jsx
+++ src/js/component/SettingsModal.jsx
@@ -1,36 +1,32 @@
 'use strict';
-//Libraries
 import React from 'react';
-// import {connect} from 'react-redux';
 import { Tabs, Tab } from 'react-bootstrap';
-
 import SettingsDurationTab from './SettingsDurationTab';
 import SettingsGreylistTab from './SettingsGreylistTab';
 
-// const firebase = require('../controllers/firebase');
-
-
 const DurationIcon = (
-  <div id="duration-tab">
+  <div>
     <i className="fa fa-clock-o"></i>
     <span>duration</span>
   </div>
-)
+);
+
 const GreylistIcon = (
-  <div id="greylist-tab">
+  <div>
     <i className="fa fa-chain-broken"></i>
     <span>greylist</span>
   </div>
-)
+);
+
 export default function SettingsModal(props) {
   return (
       <div>       
         <Tabs defaultActiveKey={1} id="settings-tabs">
           <Tab eventKey={1} title={DurationIcon}>
-            <SettingsDurationTab settings={props.settings} />
+            <SettingsDurationTab />
           </Tab>
           <Tab eventKey={2} title={GreylistIcon}>
-            <SettingsGreylistTab settings={props.settings} />
+            <SettingsGreylistTab />
           </Tab>
         </Tabs>
        </div>
