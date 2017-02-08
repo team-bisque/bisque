05a739ef150200c75dbe2f1f4cef42337f8f6f86
bcnichols3 <bcnichols3@gmail.com>
2017-02-07 20:03:59 -0500

    graph is now semi modular

--- src/js/component/Graph.jsx
+++ src/js/component/Graph.jsx
@@ -1,19 +1,42 @@
 'use strict';
 import React from 'react';
-import { connect } from 'react-redux';
-import store from '../store';
-import LineGraph from './graphs/Line'
+import LineGraph from './graphs/Line';
+import { findDOMNode } from 'react-dom';
 // import { setRouteAlias } from '../action-creators/aliases';
+
 
 export default class Graph extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      width: 500,
+      height: 500
+    };
+  }
+
+  componentDidMount() {
+    this.setSize();
+  }
+
+  componentWillUpdate(props, state) {
+    console.log('NEW WIDTH:', state.width, 'OLD WIDTH:', this.state.width);
+    if (state.width !== this.state.width) this.setSize();
+  }
+
+  setSize() {
+    const elem = findDOMNode(this);
+    this.setState({
+      width: elem.offsetWidth,
+      height: elem.offsetHeight
+    });
   }
 
   onClickClose(e){
     this.props.setRoute(null)
   }
+
   render() {
+    const {width, height} = this.state;
     return (
       <div id="graph-modal">
         <div className="modal-bar">
@@ -21,9 +44,8 @@ export default class Graph extends React.Component {
             <i className="fa fa-times" onClick={this.onClickClose.bind(this)}></i>
           </div>
         </div>
-        <LineGraph db={this.props.db} width={960} height={500} yAxis={'Words Per Minute'}/>
+        <LineGraph db={this.props.db} width={width} height={height} label={'Words Per Minute'}/>
       </div>
     );
   }
 }
-
