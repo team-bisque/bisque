7d55ca983ff6fe07a1a5ea8ac67b4cca8eabb66e
Colin Jaffe <balloonasaurus@gmail.com>
2017-02-11 13:20:26 -0500

    closes 212

--- tests/reducers/status.js
+++ tests/reducers/status.js
@@ -50,7 +50,95 @@ describe('Status Reducer ', () => {
       .to.be.deep.equal(expectedState);
   })
 
-  it('toggles pause', () => {
+  it('starts a break', () => {
+    const initialState = {
+      timeRemaining: 0,
+      isWorking: true,
+      isPaused: true,
+      durations: {
+        breakDuration: 15
+      }
+    }
+    const action = {type: types.START_BREAK};
+    const expectedState = {
+      timeRemaining: 15,
+      isWorking: false,
+      isPaused: false,
+      durations: {
+        breakDuration: 15
+      }
+    }
+    expect(reducer(initialState, action))
+      .to.be.deep.equal(expectedState);
+  });
+
+  it('starts work', () => {
+    const initialState = {
+      timeRemaining: 0,
+      isWorking: false,
+      isPaused: true,
+      durations: {
+        workDuration: 20
+      }
+    };
+    const action = {type: types.START_WORK};
+    const expectedState = {
+      timeRemaining: 20,
+      isWorking: true,
+      isPaused: false,
+      durations: {
+        workDuration: 20
+      }
+    };
+    expect(reducer(initialState, action))
+      .to.be.deep.equal(expectedState);
+  });
+
+  it('toggles work from break to work', () => {
+    const initialState = {
+      timeRemaining: 15,
+      isWorking: false,
+      isPaused: true,
+      durations: {
+        workDuration: 20
+      }
+    };
+    const action = {type: types.TOGGLE_WORK};
+    const expectedState = {
+      timeRemaining: 20,
+      isWorking: true,
+      isPaused: false,
+      durations: {
+        workDuration: 20
+      }
+    };
+    expect(reducer(initialState, action))
+      .to.be.deep.equal(expectedState);
+  });
+
+  it('toggles work from work to break', () => {
+    const initialState = {
+      timeRemaining: 25,
+      isWorking: true,
+      isPaused: true,
+      durations: {
+        breakDuration: 10
+      }
+    };
+    const action = {type: types.TOGGLE_WORK};
+    const expectedState = {
+      timeRemaining: 10,
+      isWorking: false,
+      isPaused: false,
+      durations: {
+        breakDuration: 10
+      }
+    };
+    expect(reducer(initialState, action))
+      .to.be.deep.equal(expectedState);
+  });
+
+  it('toggles pause from paused to unpaused', () => {
     const initialState = {
       isPaused: true
     };
@@ -61,5 +149,40 @@ describe('Status Reducer ', () => {
       type: types.TOGGLE_PAUSE
     }
     expect(reducer(initialState, togglePauseAction)).to.be.deep.equal(togglePauseState);
+  });
+
+  it('toggles pause from unpaused to paused', () => {
+    const initialState = {
+      isPaused: false
+    };
+    const togglePauseState = {
+      isPaused: true
+    };
+    const togglePauseAction = {
+      type: types.TOGGLE_PAUSE
+    }
+    expect(reducer(initialState, togglePauseAction)).to.be.deep.equal(togglePauseState);
+  });
+
+  it('receives durations', () => {
+    const initialState = {
+      durations: {
+        breakDuration: 10,
+        workDuration: 15,
+        lunchDuration: 30
+      }
+    };
+    const durations = {
+      breakDuration: 15,
+      workDuration: 20,
+      lunchDuration: 35
+    };
+    const expectedState = {durations};
+    const action = {
+      type: types.RECEIVE_DURATIONS,
+      durations
+    };
+    expect(reducer(initialState, action))
+      .to.be.deep.equal(expectedState);
   });
 });
